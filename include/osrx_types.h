5d3e3c3d2f52cd5a52690d0d55ba490b17f66542
LEE LapYuen <Christina_Llap_Yuen@outlook.com>
2026-04-10 16:39:14 +0800

    Make RX integer aliases host-portable

--- include/osrx_types.h
+++ include/osrx_types.h
@@ -8,15 +8,29 @@
  *   unsigned char   == 8 bits
  *   unsigned short  == 16 bits
  *   unsigned long   == 32 bits
+ *
+ * Host compilers such as LP64 Linux use 64-bit long, so select any
+ * available 32-bit base type instead of hard-coding long.
  */
+
+#include <limits.h>
 
 #ifndef OSRX_U8_DEFINED
 typedef unsigned char   osrx_u8;
 typedef unsigned short  osrx_u16;
-typedef unsigned long   osrx_u32;
 typedef signed   char   osrx_i8;
 typedef signed   short  osrx_i16;
+
+#if ULONG_MAX == 0xffffffffUL
+typedef unsigned long   osrx_u32;
 typedef signed   long   osrx_i32;
+#elif UINT_MAX == 0xffffffffU
+typedef unsigned int    osrx_u32;
+typedef signed   int    osrx_i32;
+#else
+#error "OSynaptic-RX requires a 32-bit integer type for osrx_u32/osrx_i32"
+#endif
+
 #define OSRX_U8_DEFINED 1
 #endif
 
